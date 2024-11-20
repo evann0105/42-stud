61fd257da8ecbf0a18862ee3164f9046b8a8d000
Evann Megret <evannmegret@MacBook-Pro-de-Evann.local>
2024-11-20 13:53:21 +0100

    utils

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: emgret <emegret@student.42lausanne.ch>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 12:01:18 by emgret            #+#    #+#             */
-/*   Updated: 2024/11/20 13:33:31 by emgret           ###   ########.fr       */
+/*   Updated: 2024/11/20 13:52:14 by emgret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,25 @@ char	*ft_strjoin(char *s1, char *s2)
 	size_t	s2_len;
 	size_t	i;
 
-	if (!s1 && !s2)
-		return (NULL);
-	s1_len = s1 ? ft_strlen(s1) : 0;
-	s2_len = s2 ? ft_strlen(s2) : 0;
+	if (!s2)
+		return (s1);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
 	str = malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (!str)
 		return (free(s1), NULL);
-	i = 0;
-	while (s1 && i < s1_len)
+	i = -1;
+	if (s1)
 	{
-		str[i] = s1[i];
-		i++;
+		while (++i < s1_len)
+			str[i] = s1[i];
 	}
-	while (s2 && i - s1_len < s2_len)
-	{
-		str[i] = s2[i - s1_len];
-		i++;
-	}
-	str[i] = '\0';
-	return (free(s1), str);
+	i = -1;
+	while (++i < s2_len)
+		str[s1_len + i] = s2[i];
+	str[s1_len + s2_len] = '\0';
+	free(s1);
+	return (str);
 }
 
 int	ft_strlen(char	*str)
